b420a4c67ddda4e539be69978988eeff4df078d0
amit singh <samit5585@gmail.com>
2025-10-10 14:12:06 +0530

    remove comment

--- src/main/new-home/components/Hero.jsx
+++ src/main/new-home/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
                 </div>
             </div>
 
-            {/* <ComputersCanvas /> */} //TODO: work on latter
+            {/* <ComputersCanvas /> //TODO: work on latter */}
             <div className="absolute sm:bottom-[10px] bottom-[90px] w-full flex justify-center items-center">
                 <a href="#about">
 
